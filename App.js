a3c0eeded694067a5476f7d297f8ebce3d13faf0
Soujanya-Subedi-byte <soujanya.subedi@omiba.tech>
2024-08-07 16:20:06 +0545

    react-app-practice-phase1

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  return(
+    <View style = {styles.container}> 
+      <Text style={styles.text}>
+        Hello World
+      </Text>
+      <Image source = {{uri:'https://randomuser.me/api/portraits/men/1.jpg'}} style= {styles.img} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1, 
+    justifyContent:'center', 
+    alignItems:'center',
+  },
+  text: {
+    color:'darkslateblue', 
+    fontSize:30,
+  },
+  img:{
+    width: 100,
+    height: 100,
+    borderRadius: 100/2 ,
   },
 });
+
+export default App;
